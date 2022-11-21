d3d78e39a6d71eed71a5b2c284e7db845debf1e5
Nosferas <eduardo.manczenko@gmail.com>
2022-11-21 04:19:32 -0300

    Update INAHardhat.sol

--- contracts/INAHardhat.sol
+++ contracts/INAHardhat.sol
@@ -155,7 +155,7 @@ contract accountFactory{
     }
 }
 
-    //for the code below, I will not create any test environment on hardhat (demo test environment only on INA)
+//for the code below, I will not create any test environment on hardhat (demo test environment only on INA)
 
 contract userAccount is ERC20{
     address public owner;
